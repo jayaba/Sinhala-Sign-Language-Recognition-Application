6a2c3f36827b86e37009add198826107c7b932fd
jayaba <erangajayaba0@gmail.com>
2023-04-12 01:31:33 +0530

    added more changes to homepage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -45,7 +45,107 @@ class _HomePageState extends State<HomePage> {
           Center(
             child: SignToTextTile(),
           ),
-          VoiceToSignTile()
+          VoiceToSignTile(),
+          SizedBox(
+            height: 2.5.h,
+          ),
+          Container(
+            // margin: EdgeInsets.only(left: 4.5.h),
+            margin: EdgeInsets.only(left: 1.w),
+            height: 5.h,
+            width: 93.w,
+            child: Text(
+              "Learn Sign Language",
+              style: GoogleFonts.play(fontSize: 3.5.h),
+            ),
+          ),
+          Expanded(
+              child: ListView(children: [
+            FirstLearnTile(),
+            FirstLearnTile(),
+            FirstLearnTile(),
+            FirstLearnTile(),
+            FirstLearnTile(),
+            FirstLearnTile(),
+          ]))
+        ],
+      ),
+    );
+  }
+}
+
+class FirstLearnTile extends StatelessWidget {
+  const FirstLearnTile({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 10.h,
+      width: 93.w,
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 236, 236, 236),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(
+        children: [
+          Center(
+            child: Container(
+              margin: EdgeInsets.only(left: 0.0.h),
+              height: 17.h,
+              width: 11.h,
+              child: Image(
+                image: AssetImage('assets/images/aa.png'),
+              ),
+            ),
+          ),
+          Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 1.5.h, left: 1.h),
+                height: 4.3.h,
+                width: 10.h,
+                child: Text(
+                  "අ",
+                  style: GoogleFonts.poppins(fontSize: 3.8.h),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 0.h, left: 1.h),
+                height: 2.5.h,
+                width: 11.h,
+                child: OutlinedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Learn More",
+                    style: GoogleFonts.poppins(fontSize: 1.4.h),
+                  ),
+                  style: OutlinedButton.styleFrom(
+                    primary: Color.fromARGB(255, 0, 0, 0),
+                    side: BorderSide(
+                      color: Color.fromARGB(255, 3, 3, 3),
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(7),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 11.h, bottom: 1.h),
+            width: 6.h,
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.arrow_forward_ios_outlined,
+                color: Color.fromARGB(255, 155, 152, 113),
+                size: 5.h,
+              ),
+            ),
+          )
         ],
       ),
     );
@@ -61,10 +161,10 @@ class VoiceToSignTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(top: 3.h),
-      width: 90.w,
+      width: 93.w,
       height: 20.h,
       decoration: BoxDecoration(
-        color: Color.fromARGB(255, 251, 255, 0),
+        color: Color.fromARGB(255, 231, 231, 231),
         borderRadius: BorderRadius.circular(20),
       ),
       child: Row(
@@ -73,7 +173,7 @@ class VoiceToSignTile extends StatelessWidget {
           Column(
             children: [
               Container(
-                  margin: EdgeInsets.only(right: 5.h, top: 2.h, left: 3.2.h),
+                  margin: EdgeInsets.only(right: 2.5.h, top: 2.h, left: 1.2.h),
                   child: Text(
                     "Voice To Sign",
                     style: GoogleFonts.play(fontSize: 3.5.h),
@@ -84,9 +184,9 @@ class VoiceToSignTile extends StatelessWidget {
               Container(
                 margin: EdgeInsets.only(left: 3.h),
                 height: 6.h,
-                width: 20.h,
+                width: 21.h,
                 child: Text(
-                  "This feature recognizes your sign and gives the corresponding Sinhala letters.",
+                  "This feature recognizes the user's voice and gives the corresponding Sinhala character to it.",
                   style: GoogleFonts.poppins(fontSize: 1.5.h),
                 ),
               ),
@@ -94,6 +194,7 @@ class VoiceToSignTile extends StatelessWidget {
                 height: 1.h,
               ),
               Container(
+                margin: EdgeInsets.only(right: 0.9.h),
                 height: 5.h,
                 width: 17.h,
                 child: OutlinedButton(
@@ -116,12 +217,11 @@ class VoiceToSignTile extends StatelessWidget {
             ],
           ),
           Container(
-            //     color: Colors.amber,
             margin: EdgeInsets.only(left: 1.0.h),
-            height: 15.h,
-            width: 15.h,
+            height: 17.h,
+            width: 16.h,
             child: Image(
-              image: AssetImage('assets/images/homepage_logo.png'),
+              image: AssetImage('assets/images/voiceyosigntileImage.png'),
             ),
           ),
         ],
@@ -139,10 +239,10 @@ class SignToTextTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(top: 3.h),
-      width: 90.w,
+      width: 94.w,
       height: 20.h,
       decoration: BoxDecoration(
-        color: Color.fromARGB(255, 251, 255, 0),
+        color: Color.fromARGB(255, 231, 231, 231),
         borderRadius: BorderRadius.circular(20),
       ),
       child: Row(
@@ -151,7 +251,7 @@ class SignToTextTile extends StatelessWidget {
           Column(
             children: [
               Container(
-                  margin: EdgeInsets.only(right: 5.h, top: 2.h, left: 3.2.h),
+                  margin: EdgeInsets.only(right: 2.5.h, top: 2.h, left: 1.1.h),
                   child: Text(
                     "Sign To Text",
                     style: GoogleFonts.play(fontSize: 3.5.h),
@@ -195,9 +295,10 @@ class SignToTextTile extends StatelessWidget {
           ),
           Container(
             //     color: Colors.amber,
+
             margin: EdgeInsets.only(left: 1.0.h),
-            height: 15.h,
-            width: 15.h,
+            height: 17.h,
+            width: 16.h,
             child: Image(
               image: AssetImage('assets/images/homepage_logo.png'),
             ),
